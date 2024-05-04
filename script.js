b1774d56ed4917831e9984fa8951b98d79c7b9b2
Guilherme Silva <guilrs.dev@gmail.com>
2024-05-04 19:47:48 -0300

    alteração de arquivos

--- script.js
+++ script.js
@@ -2,20 +2,25 @@ const adicionarTarefa = document.getElementById('adicionar')
 const texto = document.getElementById('input-tarefas')
 const ul = document.querySelector('ul')
 
+// Adiciona novas tarefas
 adicionarTarefa.addEventListener('click', function() {
     if (texto.value !== '') {
+        // Cria uma 'div' para armazenar os atributos da tarefa
         const divTarefa = document.createElement('div')
         divTarefa.className = 'tarefa'
 
+        // Coloca uma 'checkbox' ao lado da tarefa adicionada
         const checkbox = document.createElement('input')
         checkbox.type = 'checkbox'
         checkbox.className = 'tarefa-checkbox'
         divTarefa.appendChild(checkbox)
 
+        // Cria um 'span' que armazena o texto da tarefa adicionada
         const textoTarefa = document.createElement('span')
         textoTarefa.textContent = texto.value
         divTarefa.appendChild(textoTarefa)
 
+        // cria um 'ícone' que ficará ao lado do texto da tarefa adicionada
         const deletar = document.createElement('i')
         deletar.className = 'fa-solid fa-trash-can'
         divTarefa.appendChild(deletar)
@@ -26,12 +31,13 @@ adicionarTarefa.addEventListener('click', function() {
 
         checkbox.addEventListener('change', function() {
             if (checkbox.checked) {
-                textoTarefa.style.textDecoration = 'line-through'
+                textoTarefa.style.textDecoration = 'line-through' // Se a 'checkbox' estiver marcada, adiciona uma linha no texto da tarefa
             } else {
                 textoTarefa.style.textDecoration = 'none'
             }
         })
 
+        // Deleta a tarefa que teve seu ícone clicado
         deletar.addEventListener('click', function(e) {
             e.stopPropagation()
             ul.removeChild(divTarefa)
@@ -39,6 +45,7 @@ adicionarTarefa.addEventListener('click', function() {
     }
 })
 
+// Deleta todas as tarefas que foram adicionadas
 function excluirTarefas() {
     const tarefas = document.querySelectorAll('.tarefa');
     tarefas.forEach(function(tarefa) {
